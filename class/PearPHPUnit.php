0b4385d1e5402257b71165afb0537a4e639ab964
myname <myname@server>
2011-08-05 15:37:54 +0800

    init

---
+++ class/PearPHPUnit.php
@@ -0,0 +1,19 @@
+<?php
+namespace oc\ext\pearphpunit ;
+
+use jc\auth\IdManager;
+use jc\mvc\model\db\orm\PrototypeAssociationMap;
+use jc\db\DB ;
+use jc\db\PDODriver ;
+use oc\ext\Extension;
+
+class PearPHPUnit extends Extension
+{
+	public function load()
+	{
+		set_include_path( get_include_path().DIRECTORY_SEPARATOR.dirname(__DIR__).'/lib' ) ;
+	}
+	
+}
+
+?>
